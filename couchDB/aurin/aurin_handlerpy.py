a860890f07a120d3872b6767188e05e69e4bb074
iris-wang-22 <wzy_62250@hotmail.com>
2020-05-05 22:44:13 +1000

    aurin

---
+++ couchDB/aurin/aurin_handlerpy.py
@@ -0,0 +1,26 @@
+import json
+import time
+import sys
+
+
+boundary = json.load(open('features.json'))
+suburbs = [item['properties']['vic_loca_2'].lower() for item in boundary['features']]
+
+aurin_file = json.load(open(sys.argv[1]))
+aurin_suburbs = [item['properties'][sys.argv[4]].lower() for item in aurin_file['features']]
+aurin_data = [item['properties'][sys.argv[3]] for item in aurin_file['features']]
+aurin_list = []
+
+for suburb in suburbs:
+	for aurin_suburb in aurin_suburbs:
+		if suburb in aurin_suburb:
+			j = {}
+			j['key'] = suburb
+			j['value'] = aurin_data[aurin_suburbs.index(aurin_suburb)]
+			aurin_list.append(j)
+			break
+dictionary = {}
+dictionary['rows'] = aurin_list
+print(dictionary)
+with open(sys.argv[2], 'w') as fp:
+    json.dump(dictionary, fp)
