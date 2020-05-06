b87845790d96daea1b2d560518189e753bbc8843
Shawn <584013293@qq.com>
2020-05-06 22:36:47 +1000

    add no English pojo and controller

---
+++ backend/src/main/java/com/ccc/backend/controller/NoEnglishController.java
@@ -0,0 +1,52 @@
+package com.ccc.backend.controller;
+
+import com.ccc.backend.dao.FollowerRespository;
+import com.ccc.backend.pojo.NoEnglish;
+import com.ccc.backend.pojo.Time;
+import org.ektorp.CouchDbConnector;
+import org.ektorp.ViewQuery;
+import org.ektorp.ViewResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class NoEnglishController {
+    @Autowired
+    FollowerRespository followerRespository;
+
+    @Autowired
+    private CouchDbConnector connector;
+
+    @GetMapping(value = "/no_english")
+    public List<NoEnglish> getall(){
+        List<NoEnglish> reslut = new ArrayList<>();
+        ViewQuery query = new ViewQuery()
+                .designDocId("_design/non_english")
+                .viewName("_view").viewName("new-view").groupLevel(1);
+
+        ViewResult result = connector.queryView(query);
+        for (ViewResult.Row row : result) {
+            NoEnglish noEnglish = new NoEnglish();
+            String suburb = row.getKey();
+            suburb = suburb.substring(1,suburb.length()-1);
+            suburb = suburb.replace("\"","");
+            noEnglish.setSuburb(suburb);
+            noEnglish.setValue(Integer.parseInt(row.getValue()));
+//            System.out.println(row.getId());
+//            System.out.println(row.getKey());
+//            System.out.println(row.getValue());
+            reslut.add(noEnglish);
+        }
+
+
+
+        return reslut;
+    }
+}
