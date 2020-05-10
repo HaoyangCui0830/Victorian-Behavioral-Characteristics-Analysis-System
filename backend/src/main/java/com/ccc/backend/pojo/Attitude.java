9650710b530cdd3e503168a0d8e899eb8414b108
Shawn <584013293@qq.com>
2020-05-10 22:10:14 +1000

    add attitude pojo

---
+++ backend/src/main/java/com/ccc/backend/pojo/Attitude.java
@@ -0,0 +1,22 @@
+package com.ccc.backend.pojo;
+
+public class Attitude {
+    private String suburb;
+    private PosNegNue attitude;
+
+    public String getSuburb() {
+        return suburb;
+    }
+
+    public void setSuburb(String suburb) {
+        this.suburb = suburb;
+    }
+
+    public PosNegNue getAttitude() {
+        return attitude;
+    }
+
+    public void setAttitude(PosNegNue attitude) {
+        this.attitude = attitude;
+    }
+}
---
+++ backend/src/main/java/com/ccc/backend/pojo/PosNegNue.java
@@ -0,0 +1,31 @@
+package com.ccc.backend.pojo;
+
+public class PosNegNue {
+    private Integer pos;
+    private Integer neg;
+    private Integer neu;
+
+    public Integer getPos() {
+        return pos;
+    }
+
+    public void setPos(Integer pos) {
+        this.pos = pos;
+    }
+
+    public Integer getNeg() {
+        return neg;
+    }
+
+    public void setNeg(Integer neg) {
+        this.neg = neg;
+    }
+
+    public Integer getNeu() {
+        return neu;
+    }
+
+    public void setNeu(Integer neu) {
+        this.neu = neu;
+    }
+}
