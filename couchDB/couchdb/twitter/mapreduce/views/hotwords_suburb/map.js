78957c3af2a26f02547fb49f9c29ee13f137fb86
HAOYANG CUI <haoyangcuiqe@gmail.com>
2020-05-18 17:21:38 +1000

    update db config

--- couchDB/couchdb/twitter/mapreduce/views/hotwords_suburb/map.js
+++ couchDB/couchdb/twitter/mapreduce/views/hotwords_suburb/map.js
@@ -1,13 +1,16 @@
 function (doc) {
   if(doc.created_at != null){
     var res = doc.created_at.split(" ");
-    var date_str = res[0] + " " + res[1] + " " + res[2] + ", " + "2020 " + res[3];
+    var date_str = res[0] + " " + res[1] + " " + res[2] + ", " + res[5] + " " + res[3];
     var d = new Date(date_str);
     var n = d.getMonth();
     var m = d.getDate();
+    var p = d.getFullYear()
     if(doc.suburb!=null && doc.suburb!="None"){
       doc.entities.hashtags.forEach(function (word) {
+        if(p=="2020"){
         emit([word.text, n, m, doc.suburb], 1);
+        }
       });
     }
   }
