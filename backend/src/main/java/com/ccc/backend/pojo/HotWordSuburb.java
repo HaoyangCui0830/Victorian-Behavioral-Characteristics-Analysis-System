9c18583bf4466905fec42807889b5ef3c51e839f
Shawn <584013293@qq.com>
2020-05-20 23:35:53 +1000

    Create HotWordSuburb.java

---
+++ backend/src/main/java/com/ccc/backend/pojo/HotWordSuburb.java
@@ -0,0 +1,35 @@
+package com.ccc.backend.pojo;
+
+/**
+ * @description: TODO
+ * @author: Xin(Shawn) Wu
+ */
+public class HotWordSuburb {
+    private String word;
+    private String suburb;
+    private Integer value;
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getSuburb() {
+        return suburb;
+    }
+
+    public void setSuburb(String suburb) {
+        this.suburb = suburb;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+}
