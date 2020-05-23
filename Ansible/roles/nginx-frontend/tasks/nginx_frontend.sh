cf2ba9d1df6905317db1f9b16fb89891ffd303db
Shawn <584013293@qq.com>
2020-05-23 18:03:04 +1000

    second try

--- Ansible/roles/nginx-frontend/tasks/nginx_frontend.sh
+++ Ansible/roles/nginx-frontend/tasks/nginx_frontend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone https://HaoyangCui0830:123456github@github.com/HaoyangCui0830/CCCProject2.git
 cd CCCProject2
-cd backend
-sudo docker build -t myspringboot:latest .
-sudo docker run -p 8080:8080 -d myspringboot:latest
+cd frontend
+unzip build.zip
+sudo docker run --name nginx -v /home/ubuntu/CCCProject2/frontend/build:/usr/share/nginx/html:ro -v /home/ubuntu/nginx.conf:/etc/nginx/nginx.conf -d -p 80:80 -p 8080:8080 nginx
