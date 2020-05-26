1e73d0e6b888ebd6496fed55b7f7359b72da5658
Shawn <584013293@qq.com>
2020-05-26 22:51:49 +1000

    Update nginx_frontend.sh

--- Ansible/roles/nginx-frontend/tasks/nginx_frontend.sh
+++ Ansible/roles/nginx-frontend/tasks/nginx_frontend.sh
@@ -2,5 +2,5 @@
 git clone https://HaoyangCui0830:123456github@github.com/HaoyangCui0830/CCCProject2.git
 cd CCCProject2
 cd frontend
-unzip build.zip
+sudo unzip build.zip
 sudo docker run --name nginx -v /home/ubuntu/CCCProject2/frontend/build:/usr/share/nginx/html:ro -v /home/ubuntu/nginx.conf:/etc/nginx/nginx.conf -d -p 80:80 -p 8080:8080 nginx
